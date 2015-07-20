698dd4605b333e8d19d5ed69df959b131dd61459
AlbertWigmore <albert@albertwigmore.co.uk>
2015-07-20 19:56:36 +0100

    Added Bulk Data Creation for Orbital Elements
    Data insertion was taking too long so orbital elements has been switched
    to bulk creation format. Also had issues with MySQL net buffer size when
    inserting large data

--- KeplerSatellites/management/commands/satellites_readin.py
+++ KeplerSatellites/management/commands/satellites_readin.py
@@ -1,40 +1,15 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime
-import json
-import httplib
-from django.core.serializers.json import DjangoJSONEncoder
 from KeplerSatellites.models import *
-from django.conf import settings
+from query import query
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        base = "www.space-track.org"
-        auth = "/ajaxauth/login"
-
-        norad_id_list = range(130, 40000, 1)
+        norad_id_list = range(39, 40000, 1)
         print norad_id_list
         for norad_id in norad_id_list:
-            # Returns tle data for norad id, used for orbital elements
-            tle_query = "https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/"+str(norad_id)+"/metadata/false/distinct/true"
-
-            # Returns data from satcat for norad id, only used for launch data
-            satcat_query = "https://www.space-track.org/basicspacedata/query/class/satcat/NORAD_CAT_ID/"+str(norad_id)+"/metadata/false"
-
-            jsonquery = {'identity': settings.USERNAME, 'password': settings.PASSWORD, 'query': satcat_query}
-            headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
-
-            conn = httplib.HTTPSConnection(base)
-            conn.request("POST", auth, json.dumps(jsonquery, cls=DjangoJSONEncoder), headers)
-
-            try:
-                response = conn.getresponse()
-                satellite_json = response.read()
-            except:
-                satellite_json = None
-
-            conn.close()
 
             site_dict = {
                 'AFETR': "Air Force Eastern Test Range",
@@ -69,98 +44,79 @@ class Command(BaseCommand):
                 'YUN':	 "Yunsong, DPRK",
                 None:    None,
             }
-            try:
-                satellite_cat = json.loads(satellite_json)
+            #INSERT QUERY FUNCTION
+            satellite_cat = query("https://www.space-track.org/basicspacedata/query/class/satcat/NORAD_CAT_ID/"+str(norad_id)+"/metadata/false")
 
-                if satellite_cat[0].get(u'DECAY', None) is None:
-                    launch = None
-                else:
-                    launch = datetime.strptime(satellite_cat[0].get(u'DECAY'), "%Y-%m-%d")
+            if satellite_cat[0].get(u'DECAY', None) is None:
+                launch = None
+            else:
+                launch = datetime.strptime(satellite_cat[0].get(u'DECAY'), "%Y-%m-%d")
 
-                if satellite_cat[0].get(u'LAUNCH', None) is None:
-                    decay = None
-                else:
-                    decay = datetime.strptime(satellite_cat[0].get(u'LAUNCH', None), "%Y-%m-%d")
-                satellite_dict = {
-                    'norad_id': (satellite_cat[0].get(u'NORAD_CAT_ID', None)),
-                    'sat_name': (satellite_cat[0].get(u'SATNAME', None)),
-                    'site_code': (satellite_cat[0].get(u'SITE', None)),
-                    'launch': launch,
-                    'site': site_dict[(satellite_cat[0].get(u'SITE', None))],
-                    'decay': decay,
-                    'rcs_value': (satellite_cat[0].get(u'RCSVALUE', None)),
-                    'rcs_size': (satellite_cat[0].get(u'RCS_SIZE', None)),
-                    'elset_classification': (satellite_cat[0].get(u'CLASSIFICATION_TYPE', None)),
-                    'international_designator': (satellite_cat[0].get(u'INTLDES', None)),
-                    'launch_year': (satellite_cat[0].get(u'LAUNCH_YEAR', None)),
-                    'launch_num': (satellite_cat[0].get(u'LAUNCH_NUM', None)),
-                    'launch_piece': (satellite_cat[0].get(u'LAUNCH_PIECE', None)),
-                    'current': (satellite_cat[0].get(u'CURRENT', None)),
-                    'comment': (satellite_cat[0].get(u'COMMENT', None)),
-                    'originator': (satellite_cat[0].get(u'ORIGINATOR', None)),
+            if satellite_cat[0].get(u'LAUNCH', None) is None:
+                decay = None
+            else:
+                decay = datetime.strptime(satellite_cat[0].get(u'LAUNCH', None), "%Y-%m-%d")
+            satellite_dict = {
+                'norad_id': (satellite_cat[0].get(u'NORAD_CAT_ID', None)),
+                'sat_name': (satellite_cat[0].get(u'SATNAME', None)),
+                'site_code': (satellite_cat[0].get(u'SITE', None)),
+                'launch': launch,
+                'site': site_dict[(satellite_cat[0].get(u'SITE', None))],
+                'decay': decay,
+                'rcs_value': (satellite_cat[0].get(u'RCSVALUE', None)),
+                'rcs_size': (satellite_cat[0].get(u'RCS_SIZE', None)),
+                'elset_classification': (satellite_cat[0].get(u'CLASSIFICATION_TYPE', None)),
+                'international_designator': (satellite_cat[0].get(u'INTLDES', None)),
+                'launch_year': (satellite_cat[0].get(u'LAUNCH_YEAR', None)),
+                'launch_num': (satellite_cat[0].get(u'LAUNCH_NUM', None)),
+                'launch_piece': (satellite_cat[0].get(u'LAUNCH_PIECE', None)),
+                'current': (satellite_cat[0].get(u'CURRENT', None)),
+                'comment': (satellite_cat[0].get(u'COMMENT', None)),
+                'originator': (satellite_cat[0].get(u'ORIGINATOR', None)),
 
-                    'country': (satellite_cat[0].get(u'', None)),
-                }
-
-            except ValueError as json_error:
-                satellite_cat = None
-                satellite_dict = None
-                print json_error
+                'country': (satellite_cat[0].get(u'', None)),
+            }
 
             try:
                 satellite_foreign_key = Satellite.objects.update_or_create(
                     norad_id=(satellite_cat[0].get(u'NORAD_CAT_ID', None)), defaults=satellite_dict)
-                print "Satellite Added " + str(norad_id)
+                print "Satellite "+str(norad_id)+" Created"
             except Satellite.DoesNotExist:
                 satellite_foreign_key = None
                 print Satellite.DoesNotExist
 
-            jsonquery = {'identity': settings.USERNAME, 'password': settings.PASSWORD, 'query': tle_query}
-            headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
+            # INSERT QUERY
+            satellite_tle = query("https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/"+str(norad_id)+"/metadata/false/distinct/true")
 
-            conn = httplib.HTTPSConnection(base)
-            conn.request("POST", auth, json.dumps(jsonquery, cls=DjangoJSONEncoder), headers)
-
-            try:
-                response = conn.getresponse()
-                satellite_json = response.read()
-            except:
-                satellite_json = None
-
-            conn.close()
-            try:
-                satellite_tle = json.loads(satellite_json)
-            except ValueError as json_error:
-                    print json_error
-
+            bulk_data = []
             j = 0
             for x in satellite_tle:
-                orbital_elements_dict = {
-                    'period': (satellite_tle[j].get(u'PERIOD', None)),
-                    'inclination': (satellite_tle[j].get(u'INCLINATION', None)),
-                    'apogee': (satellite_tle[j].get(u'APOGEE', None)),
-                    'perigee': (satellite_tle[j].get(u'PERIGEE', None)),
-                    'RAAN': (satellite_tle[j].get(u'RA_OF_ASC_NODE', None)),
-                    'eccentricity': (satellite_tle[j].get(u'ECCENTRICITY', None)),
-                    'argument_of_perigee': (satellite_tle[j].get(u'ARG_OF_PERICENTER', None)),
-                    'object_type': (satellite_tle[j].get(u'OBJECT_TYPE', None)),
-                    'mean_anomaly': (satellite_tle[j].get(u'MEAN_ANOMALY', None)),
-                    'mean_motion': (satellite_tle[j].get(u'MEAN_MOTION', None)),
-                    'rev_number_at_epoch': (satellite_tle[j].get(u'REV_AT_EPOCH', None)),
-                    'first_derivative_of_mean_motion': (satellite_tle[j].get(u'MEAN_MOTION_DOT', None)),
-                    'second_derivative_of_mean_motion': (satellite_tle[j].get(u'MEAN_MOTION_DDOT', None)),
-                    'b_drag_term': (satellite_tle[j].get(u'BSTAR', None)),
-                    'element_set_epoch': (satellite_tle[j].get(u'ELEMENT_SET_NO', None)),
-                    'tle_line_1': (satellite_tle[j].get(u'TLE_LINE1', None)),
-                    'tle_line_2': (satellite_tle[j].get(u'TLE_LINE2', None)),
-                    'epoch': datetime.strptime(satellite_tle[j].get(u'EPOCH', None), "%Y-%m-%d %H:%M:%S"),
-                    'epoch_microsecond': (satellite_tle[j].get(u'EPOCH_MICROSECONDS', None)),
-                    'satellite': satellite_foreign_key[0],
-                }
-
-                try:
-                    OrbitalElements.objects.update_or_create(epoch=datetime.strptime(satellite_tle[j].get(u'EPOCH', None), "%Y-%m-%d %H:%M:%S"), defaults=orbital_elements_dict)
-                    print "Orbital Elements Added " + str(j)
-                except OrbitalElements.DoesNotExist:
-                    print OrbitalElements.DoesNotExist
+                bulk_data.append(OrbitalElements(
+                    period=(satellite_tle[j].get(u'PERIOD', None)),
+                    inclination=(satellite_tle[j].get(u'INCLINATION', None)),
+                    apogee=(satellite_tle[j].get(u'APOGEE', None)),
+                    perigee=(satellite_tle[j].get(u'PERIGEE', None)),
+                    RAAN=(satellite_tle[j].get(u'RA_OF_ASC_NODE', None)),
+                    eccentricity=(satellite_tle[j].get(u'ECCENTRICITY', None)),
+                    argument_of_perigee=(satellite_tle[j].get(u'ARG_OF_PERICENTER', None)),
+                    object_type=(satellite_tle[j].get(u'OBJECT_TYPE', None)),
+                    mean_anomaly=(satellite_tle[j].get(u'MEAN_ANOMALY', None)),
+                    mean_motion=(satellite_tle[j].get(u'MEAN_MOTION', None)),
+                    rev_number_at_epoch=(satellite_tle[j].get(u'REV_AT_EPOCH', None)),
+                    first_derivative_of_mean_motion=(satellite_tle[j].get(u'MEAN_MOTION_DOT', None)),
+                    second_derivative_of_mean_motion=(satellite_tle[j].get(u'MEAN_MOTION_DDOT', None)),
+                    b_drag_term=(satellite_tle[j].get(u'BSTAR', None)),
+                    element_set_epoch=(satellite_tle[j].get(u'ELEMENT_SET_NO', None)),
+                    tle_line_1=(satellite_tle[j].get(u'TLE_LINE1', None)),
+                    tle_line_2=(satellite_tle[j].get(u'TLE_LINE2', None)),
+                    epoch=datetime.strptime(satellite_tle[j].get(u'EPOCH', None), "%Y-%m-%d %H:%M:%S"),
+                    epoch_microsecond=(satellite_tle[j].get(u'EPOCH_MICROSECONDS', None)),
+                    satellite=satellite_foreign_key[0],
+                ))
+                print "Orbital Element "+str(j)+" Added"
                 j += 1
+            try:
+                OrbitalElements.objects.bulk_create(bulk_data)
+                print "Orbital Elements Created"
+            except OrbitalElements.DoesNotExist:
+                print OrbitalElements.DoesNotExist
